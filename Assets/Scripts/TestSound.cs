e2f48a85a7d82e12d988f2427a597ed8ad2021da
leejw1212 <jwleekw@naver.com>
2021-08-10 22:34:17 +0900

    SoundManager #2
    move Sound directory
    Add define.cs Sound enum
    Add TestSound.cs testcode
    Add SoundManager.cs Main Logic
    Add Managers.cs SoundManager init

--- Assets/Scripts/TestSound.cs
+++ Assets/Scripts/TestSound.cs
@@ -21,11 +21,13 @@ public class TestSound : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        AudioSource audio = GetComponent<AudioSource>();
+        //AudioSource audio = GetComponent<AudioSource>();
+        //audio.PlayOneShot(audioClip);
+        //audio.PlayOneShot(audioClip2);
+        //float lifeTime = Mathf.Max(audioClip.length, audioClip2.length);
+        //GameObject.Destroy(gameObject, lifeTime);
 
-        audio.PlayOneShot(audioClip);
-        audio.PlayOneShot(audioClip2);
-        float lifeTime = Mathf.Max(audioClip.length, audioClip2.length);
-        GameObject.Destroy(gameObject, lifeTime);
+        Managers.Sound.Play(Define.Sound.Effect, "UnityChan/univ0001");
+        Managers.Sound.Play(Define.Sound.Effect, "UnityChan/univ0002");
     }
 }
