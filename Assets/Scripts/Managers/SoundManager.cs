cff1baaa21703c1b7bf6ff065a2e45388be8ec76
leejw1212 <jwleekw@naver.com>
2021-08-10 22:11:28 +0900

    SoundManager #1
    Add SoundManager
    Add TestSound

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager 
+{
+    //mp3 player -> audio source
+    //mp3 music -> audioclip
+    //관객 -> audolistener
+}
