bd2a6c8363c575eb5fc8bccbd39bb10c115f9135
leejw1212 <jwleekw@naver.com>
2021-08-12 00:36:48 +0900

    Pool Manager #1
    Add Poolable.cs
    Mod Managers.cs
    Add PoolManager.cs
    Mod ResourceManager.cs

--- Assets/Scripts/Managers/ResourceManager.cs
+++ Assets/Scripts/Managers/ResourceManager.cs
@@ -11,6 +11,8 @@ public class ResourceManager
 
     public GameObject Instantiate(string path, Transform parent = null)
     {
+
+        // 1. 오리지널 이미 들고 있으면 바로 사용
         GameObject prefab = Load<GameObject>($"Prefabs/{path}");
         if(prefab == null)
         {
@@ -18,12 +20,9 @@ public class ResourceManager
             return null;
         }
 
+        // 2. 혹시 풀링된 애가 있을까?
         GameObject go = Object.Instantiate(prefab, parent);
-        int index = go.name.IndexOf("(Clone)");
-        if(index > 0)
-        {
-            go.name = go.name.Substring(0, index);
-        }
+        go.name = prefab.name;
 
         return go;
     }
@@ -32,6 +31,8 @@ public class ResourceManager
     {
         if (go == null)
             return;
+
+        // 만약 풀링이 필요한 아이라면 -> 풀링 매니저한테 위탁
 
         Object.Destroy(go);
     }
