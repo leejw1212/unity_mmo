ddc71e9b50fa338caa498aa24f533b4969b31ffa
leejw1212 <jwleekw@naver.com>
2021-08-13 07:59:43 +0900

    Coroutine  #1

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
+    class Test
+    {
+        public int Id = 0;
+
+    }
+
+    class CoroutineTest : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < 100000000; i++)
+            {
+                if (i % 100000 == 0)
+                    yield return null;
+            }
+        }
+    }
+
     protected override void Init()
     {
         base.Init();
@@ -12,8 +30,7 @@ public class GameScene : BaseScene
 
         Managers.UI.ShowSceneUI<UI_Inven>();
 
-        for (int i = 0; i < 2; i++)
-            Managers.Resource.Instantiate("UnityChan");
+        CoroutineTest test = new CoroutineTest();
     }
 
     public override void Clear()
