58d9f5da1028f3733b4043dbf315e73ef201fa68
leejw1212 <jwleekw@naver.com>
2021-08-14 01:01:56 +0900

    Coroutine #2
    Mod GameScene

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -4,23 +4,7 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
-    class Test
-    {
-        public int Id = 0;
-
-    }
-
-    class CoroutineTest : IEnumerable
-    {
-        public IEnumerator GetEnumerator()
-        {
-            for (int i = 0; i < 100000000; i++)
-            {
-                if (i % 100000 == 0)
-                    yield return null;
-            }
-        }
-    }
+    Coroutine co;
 
     protected override void Init()
     {
@@ -30,7 +14,28 @@ public class GameScene : BaseScene
 
         Managers.UI.ShowSceneUI<UI_Inven>();
 
-        CoroutineTest test = new CoroutineTest();
+        co = StartCoroutine("ExplodeAfterSeconds", 4.0f);
+        StartCoroutine("CoStopExplode", 2.0f);
+    }
+
+    IEnumerator CoStopExplode(float seconds)
+    {
+        Debug.Log("Stop Enter");
+        yield return new WaitForSeconds(seconds);
+        Debug.Log("Stop Execute");
+        if (co != null)
+        {
+            StopCoroutine(co);
+            co = null;
+        }
+    }
+
+    IEnumerator ExplodeAfterSeconds(float seconds)
+    {
+        Debug.Log("Explode Enter");
+        yield return new WaitForSeconds(seconds);
+        Debug.Log("Explode Execute");
+        co = null;
     }
 
     public override void Clear()
