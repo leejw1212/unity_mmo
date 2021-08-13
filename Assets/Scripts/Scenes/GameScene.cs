2b97ec51eadde74ae209a81658645ad331aae0c9
leejw1212 <jwleekw@naver.com>
2021-08-14 01:45:36 +0900

    Data Manager #1
    Add DataManager
    Add StatData.json

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
-    Coroutine co;
-
     protected override void Init()
     {
         base.Init();
@@ -13,29 +11,6 @@ public class GameScene : BaseScene
         SceneType = Define.Scene.Game;
 
         Managers.UI.ShowSceneUI<UI_Inven>();
-
-        co = StartCoroutine("ExplodeAfterSeconds", 4.0f);
-        StartCoroutine("CoStopExplode", 2.0f);
-    }
-
-    IEnumerator CoStopExplode(float seconds)
-    {
-        Debug.Log("Stop Enter");
-        yield return new WaitForSeconds(seconds);
-        Debug.Log("Stop Execute");
-        if (co != null)
-        {
-            StopCoroutine(co);
-            co = null;
-        }
-    }
-
-    IEnumerator ExplodeAfterSeconds(float seconds)
-    {
-        Debug.Log("Explode Enter");
-        yield return new WaitForSeconds(seconds);
-        Debug.Log("Explode Execute");
-        co = null;
     }
 
     public override void Clear()
