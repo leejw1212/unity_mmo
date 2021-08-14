e62150a9d48b62c4d46ad3ff0ace0669a717d6ab
leejw1212 <jwleekw@naver.com>
2021-08-14 19:07:19 +0900

    Setting & Moving
    SET Environment
    Add Moving

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -37,18 +38,26 @@ public class PlayerController : MonoBehaviour
     void UpdateMoving()
     {
         Vector3 dir = _destPos - transform.position;
-        if (dir.magnitude < 0.0001f)
+        if (dir.magnitude < 0.1f)
         {
             _state = PlayerState.Idle;
         }
         else
         {
+            NavMeshAgent nma = gameObject.GetOrAddComponent<NavMeshAgent>();
+
             float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
 
-            transform.position += dir.normalized * moveDist;
+            nma.Move(dir.normalized * moveDist);
 
+            Debug.DrawRay(transform.position, dir.normalized, Color.green);
+            if(Physics.Raycast(transform.position + Vector3.up * 0.5f , dir, 1.0f, LayerMask.GetMask("Block")))
+            {
+                _state = PlayerState.Idle;
+                return;
+            }
+            
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);
-            transform.LookAt(_destPos);
         }
 
         //애니매이션
@@ -124,7 +133,7 @@ public class PlayerController : MonoBehaviour
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+        //Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
 
         //int mask = (1 << 8) | (1 << 9); // layer 설정한 것만 raycast 되도록 설정
                                         //LayerMask mask = LayerMask.GetMask("Monster") | LayerMask.GetMask("wall");
